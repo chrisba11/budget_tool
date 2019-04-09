ef836b00e4d6e14596d3dea3d08423e536c096f0
Chris Ball <chrisball7@gmail.com>
2019-04-09 15:26:18 -0700

    working on model classes

--- budgets_app/models.py
+++ budgets_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.utils import timezone
+
+
+class Budget(models.Model):
+    """
+
+    """
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='budgets',
+        )
+
+    name = models.CharField(
+        max_length=255,
+        default='That Which Shall Not Be Named',
+        )
+
+    total_budget = models.FloatField(
+        default=0
+    )
+
+    remaining_balance = models.FloatField(
+        
+    )
 
 # Create your models here.
--- budgets_project/settings.py
+++ budgets_project/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split()
 # Application definition
 
 INSTALLED_APPS = [
+    'budgets_project',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'budgets_project',
+    'budgets_app',
 ]
 
 MIDDLEWARE = [
